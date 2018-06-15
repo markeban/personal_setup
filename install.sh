4831142e6fee37b9f8f41cadae24842d26155982
Mark Richardson <mark.richardson@gamut.com>
2018-06-15 11:48:04 -0500

    read plist file for iterm

--- install.sh
+++ install.sh
@@ -10,6 +10,7 @@ brew bundle check
 # load iterm config
 sudo defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/personal_setup"
 sudo defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
+sudo defaults read com.googlecode.iterm2.plist
 
 # create bash_profile and load config
 if [! -f ~/.bash_profile ]; then
@@ -21,4 +22,4 @@ cat ./dot_files/bash_profile >> ~/.bash_profile
 sudo cat ./spectacle_settings.json > ~/Library/Application Support/Spectacle/Shortcuts.json
 
 #install sublime text
-cp -Rf ./sublime_settings/ ~/Library/Application Support/Sublime Text 3/Packages/User
+cp -Rf ./sublime_settings/ ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/
